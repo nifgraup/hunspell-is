31ae165567678d66692edc358610e37074a3d8ef
Björgvin Ragnarsson <nifgraup@gmail.com>
2011-11-23 20:59:06 +0100

    updated makedict to work with one rule per directory

--- makedict.sh
+++ makedict.sh
@@ -52,17 +52,17 @@ elif [ "$1" = "test" ]; then
     exit 1
   fi
   echo "Testing rules..."
-  find langs/$2 -type f|grep "\.aff$" |while read i
+  find langs/$2/* -type d | while read i
   do
     cp langs/$2/common-aff ${TMP}/huntest.aff
-    LINECOUNT="`grep -cve '^\s*$' "$i"`"
+    LINECOUNT="`grep -cve '^\s*$' "$i/aff"`"
     echo "SFX X N $LINECOUNT" >> ${TMP}/huntest.aff
-    cat "$i" >> ${TMP}/huntest.aff
-    TESTNAME="`basename "$i" .aff`"
+    cat "$i/aff" >> ${TMP}/huntest.aff
+    TESTNAME="`basename "$i"`"
     echo "Testing rule $TESTNAME"
-    cp "langs/$2/$TESTNAME.dic" ${TMP}/huntest.dic
-    test -z "`hunspell -l -d ${TMP}/huntest < "langs/$2/$TESTNAME.good"`" || { echo "Good word test for $TESTNAME failed: `hunspell -l -d ${TMP}/huntest < "langs/$2/$TESTNAME.good"`"; exit 1; }
-    test -z "`hunspell -G -d ${TMP}/huntest < "langs/$2/$TESTNAME.bad"`" || { echo "Bad word test for $TESTNAME failed: `hunspell -G -d ${TMP}/huntest < "langs/$2/$TESTNAME.bad"`"; exit 1; }
+    cp "$i/dic" ${TMP}/huntest.dic
+    test -z "`hunspell -l -d ${TMP}/huntest < "$i/good"`" || { echo "Good word test for $TESTNAME failed: `hunspell -l -d ${TMP}/huntest < "$i/good"`"; exit 1; }
+    test -z "`hunspell -G -d ${TMP}/huntest < "$i/bad"`" || { echo "Bad word test for $TESTNAME failed: `hunspell -G -d ${TMP}/huntest < "$i/bad"`"; exit 1; }
   done
   echo "All passed."
 
@@ -93,22 +93,22 @@ elif [ "$1" != "" ]; then
   cp langs/$1/common-aff dicts/$1.aff
   FLAG=`grep -o [[:space:]][[:digit:]]*[[:space:]]N[[:space:]] langs/$1/common-aff | gawk 'BEGIN {max = 0} {if($1>max) max=$1} END {print max}'`
 
-  find langs/$1 -type f|grep "\.aff$" |while read i
+  find langs/$1/* -type d | while read i
   do
     FLAG=`expr $FLAG + 1`
-    RULE="`basename "$i" .aff`"
-    LINECOUNT="`grep -cve '^\s*$' "$i"`"
+    RULE="`basename "$i"`"
+    LINECOUNT="`grep -cve '^\s*$' "$i/aff"`"
     echo "   Extracting rule $RULE"
 
     echo "#$RULE" >> dicts/$1.aff
     echo "SFX $FLAG N $LINECOUNT" >> dicts/$1.aff
-    cat "$i" | sed "s/SFX X/SFX $FLAG/g" >> dicts/$1.aff
+    cat "$i/aff" | sed "s/SFX X/SFX $FLAG/g" >> dicts/$1.aff
 
-    if [ -e "langs/$1/$RULE.print-dic-entry" ]; then
-        grep -o "^{{$RULE|[^}]\+" ${TMP}/${1}wiktionary-latest-pages-articles.xml.texts | grep -o "|.*" | "./langs/${1}/$RULE.print-dic-entry" $FLAG >> ${TMP}/wiktionary.extracted
+    if [ -e "$i/print-dic-entry" ]; then
+        grep -o "^{{$RULE|[^}]\+" ${TMP}/${1}wiktionary-latest-pages-articles.xml.texts | grep -o "|.*" | "./$i/print-dic-entry" $FLAG >> ${TMP}/wiktionary.extracted
     else
-        if [ -e "langs/$1/$RULE.format" ]; then
-    	    REFORMATSTRING="`cat "langs/$1/$RULE.format"`"
+        if [ -e "$i/format" ]; then
+    	    REFORMATSTRING="`cat "$i/format"`"
         else
 	    REFORMATSTRING="%s%s%s"
         fi
