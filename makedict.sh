918e3709930b605cf244ae248113f67ed0159784
Björgvin Ragnarsson <nifgraup@gmail.com>
2011-11-30 20:13:02 +0100

    added rule Fallbeyging kvk vb 01a

--- makedict.sh
+++ makedict.sh
@@ -4,6 +4,7 @@
 # License: Public Domain
 
 #todo:
+#reorder common rules
 #tools/insert-common-rule
 #Remove words containing (fornt) and skáldamál
 #check if unconfirmed revision of pages end upp in the dictionary
