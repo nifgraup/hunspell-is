ea5249dd7b3c9144703e72d0c51ef4d90aa02f7b
Björgvin Ragnarsson <nifgraup@gmail.com>
2011-12-05 00:13:34 +0100

    removed words with = and spaces in them.

--- makedict.sh
+++ makedict.sh
@@ -89,7 +89,7 @@ elif [ "$1" = "packages" ]; then
 elif [ "$1" != "" ]; then
   echo "Downloading files..."
   test -e ${TMP}/${1}wiktionary-latest-pages-articles.xml || wget --timestamping http://download.wikimedia.org/${1}wiktionary/latest/${1}wiktionary-latest-pages-articles.xml.bz2 -O - | bunzip2 > ${TMP}/${1}wiktionary-latest-pages-articles.xml
-  test -e ${TMP}/${1}wiktionary-latest-pages-articles.xml.texts || grep -o "{{[^.]*|[^-.][^}]*" ${TMP}/iswiktionary-latest-pages-articles.xml | sort | uniq > ${TMP}/iswiktionary-latest-pages-articles.xml.texts
+  test -e ${TMP}/${1}wiktionary-latest-pages-articles.xml.texts || grep -o "{{[^.]*|[^-.][^}]*" ${TMP}/iswiktionary-latest-pages-articles.xml | grep -v "{{.*|.*[ =]" | sort | uniq > ${TMP}/iswiktionary-latest-pages-articles.xml.texts
   echo "Extracting valid words from the wiktionary dump..."
   rm -f ${TMP}/wiktionary.extracted
   mkdir -p dicts
