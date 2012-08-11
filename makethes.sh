e95b404232687b120f3c5db9044f5937cc894e89
xubuntu <xubuntu@xubuntu-VirtualBox.(none)>
2012-08-11 11:18:08 -0400

    remove all tags, not just super-script, add todo list.

--- makethes.sh
+++ makethes.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+#Todo:
+#	Laga formatting villur í Wiktionary
+#	Gera Makefile sem skilgreining dependencies, m.a. fyrir makedict.sh packages is
+#	Flokka eftir [1], [2] etc. (Viljum við gera það ef t.d. eru tvö [1] í sama *heiti eða bara á milli heita?)
+#	Bæta við orðskýringum
+#		ef þær eru fleiri en ein og eitt orð hver?
+#		ef þær eru ekki nafnorð, þ.á.m. sérnöfn, karlmanns- og kvenmannsnöfn, (og fleiri en eitt)?
+#	bæta við öðrum stafsetningum, andheitum, yfirheitum, undirheitum, "sjá einnig", (orðtök, "afleiddar merkingar" og orðsifjar).
+#		Hvað með orð í fleiri en einum orðflokki, t.d. dýr?
+
 TMP=tmp
 LANG=is
 
@@ -30,7 +40,7 @@ LC_ALL=is_IS.utf8 gawk -F " " '
 			thes = $0;
 			sub(/:+(\[([[:alnum:]]|,|-)+\])? */, "", thes);
 			gsub(/\[\[(([[:alnum:]]| )*\|)?|\]/, "", thes); #remove links
-			gsub(/ *&lt;sup&gt;[^;]*&lt;\/sup&gt;/, "", thes); #remove super-scripted text
+			gsub(/ *&lt;[[:alpha:]]+&gt;[^;]*&lt;\/[[:alpha:]]+&gt;/, "", thes); #remove html tags and text inside.
 			gsub(/, */, "|", thes); #todo: ekki skipta út í texta sem fer í sviga
 			while(match(thes, /\047|{|}|:/))
 			{
